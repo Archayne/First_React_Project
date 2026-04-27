a4d8582e7492bced92918cbd7b734bab536cf813
Archayne <hargrovea31@gmail.com>
2026-04-27 11:57:31 -0400

    made coreconcepts component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcepts/CoreConcept';
 import TabButton from './components/TabButton/TabButton';
 import { useState } from 'react';
+import CoreConcepts from './components/CoreConcepts/CoreConcepts';
 
 
 function App() {
@@ -29,17 +30,10 @@ function App() {
     console.log(selectedTopic);
   }
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id='core-concepts'>
-          <h2>Time to get started!</h2>
-        <ul>
-          {
-          CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />) 
-          }
-        </ul>
-        </section>
+        <CoreConcepts />
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
@@ -51,7 +45,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
